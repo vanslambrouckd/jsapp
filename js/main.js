434eb2748ec641a4ad0817778fcb28260001fa57
david vanslambrouck <vanslambrouckd@users.noreply.github.com>
2014-12-08 23:21:48 +0100

    -

--- js/main.js
+++ js/main.js
@@ -4,5 +4,7 @@ https://github.com/addyosmani/largescale-demo
 https://github.com/addyosmani/essential-js-design-patterns
 http://addyosmani.com/writing-modular-js/
 http://addyosmani.com/largescalejavascript/
+
+http://code.tutsplus.com/tutorials/writing-modular-javascript--net-14746
  */
 var app = app || {};
